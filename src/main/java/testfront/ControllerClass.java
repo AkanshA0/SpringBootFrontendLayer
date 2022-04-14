258525908820e621aa8c36b0a0b84a5db6a77bab
AkanshA0 <akkansha01@gmail.com>
2022-04-14 17:42:51 +0530

    init

---
+++ src/main/java/testfront/ControllerClass.java
@@ -0,0 +1,13 @@
+package testfront;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ControllerClass {
+	@RequestMapping("/p")
+	public String homePage()
+	{
+		return "homepage";
+	}
+}  
